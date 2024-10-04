573554bfd59e093b9f0763305f418155c7f082df
Park Seung Ho <dreamhigh924@gmail.com>
2024-10-04 23:55:44 +0900

    refacor: Api 경로 수정

--- spring/src/main/java/poe/spring/domain/member/controller/EmailMemberController.java
+++ spring/src/main/java/poe/spring/domain/member/controller/EmailMemberController.java
@@ -10,18 +10,65 @@ import poe.spring.domain.member.dto.ResponseDto;
 import poe.spring.domain.member.service.MemberCrudService;
 import poe.spring.domain.member.dto.RequestDto;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/user/{id}/update")
+@RequestMapping
 @RequiredArgsConstructor
-public class UpdateController {
+public class EmailMemberController {
 
     private final MemberCrudService crudService;
     private final MapConverter<ResponseDto> mapConverter;
 
-    // TODO API 설계 변경: email, password, phoneNumber 변경 각각 다른 API로 처리
-    @PatchMapping("/email")
+    @PostMapping("/email-user")
+    public ResponseEntity<Api<Map<String, Long>>> createEmailUser(@RequestBody RequestDto requestDto) {
+
+        Long id = crudService.createUser(requestDto);
+
+        Map<String, Long> data = new HashMap<>();
+        data.put("member_id", id);
+
+        Api<Map<String, Long>> response = Api.<Map<String, Long>>builder()
+                .data(data)
+                .statusCode(String.valueOf(HttpStatus.CREATED.value()))
+                .resultMessage("Created a new member successfully.").build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @DeleteMapping("/email-user/{id}")
+    public ResponseEntity<Api<Void>> deleteEmailUser(@PathVariable Long id) {
+
+        // 사용자 삭제 로직
+        crudService.deleteUser(id);
+
+        Api<Void> response = Api.<Void>builder()
+                .statusCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage("Member deleted successfully.").build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @GetMapping("/email-user/{id}")
+    public ResponseEntity<Api<Map<String, Object>>> readEmailUser(@PathVariable Long id) {
+
+        // 사용자 정보 조회 로직
+        ResponseDto responseDto = crudService.readUser(id);
+
+        MapConverter<ResponseDto> mapConverter = new MapConverter<>();
+        Map<String, Object> data = mapConverter.convertToMap(responseDto);
+
+        Api<Map<String, Object>> response = Api.<Map<String, Object>>builder()
+                .statusCode(HttpStatus.OK.getReasonPhrase())
+                .resultMessage("Successfully read member information")
+                .data(data)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @PatchMapping("/email-user/{id}/email")
     public ResponseEntity<Api<Map<String, Object>>> updateUserEmail(
             @PathVariable Long id,
             @RequestBody RequestDto requestDto) {
@@ -40,7 +87,7 @@ public class UpdateController {
                 .body(response);
     }
 
-    @PatchMapping("/password")
+    @PatchMapping("/email-user/{id}/password")
     public ResponseEntity<Api<Void>> updateUserPassword(
             @PathVariable Long id,
             @RequestBody RequestDto requestDto) {
@@ -55,7 +102,7 @@ public class UpdateController {
                 .body(response);
     }
 
-    @PatchMapping("/phone")
+    @PatchMapping("/email-user/{id}/phone")
     public ResponseEntity<Api<Map<String, Object>>> updateUserPhone(
             @PathVariable Long id,
             @RequestBody RequestDto requestDto) {
@@ -73,4 +120,5 @@ public class UpdateController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(response);
     }
+
 }
